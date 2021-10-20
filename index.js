d9ab5813a294161d47ae6bd2c8c2a13922e732d0
mmatagne <miromanuel.matagne@gmail.com>
2021-10-20 11:18:41 +0200

    private messaging

--- index.js
+++ index.js
@@ -1,50 +1,74 @@
-const { SocketAddress } = require('net');
+const { SocketAddress } = require("net");
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 const connectedUsers = {};
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', socket => {
-  console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  connectedUsers[socket.id] = "user";
+  io.emit("chat message", "Nuevo usuario conectado");
+  console.log(connectedUsers);
 
-  connectedUsers[socket.id] = 'user';
-  io.emit('chat message', 'Nuevo usuario conectado');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
     delete connectedUsers[socket.id];
   });
 
-  socket.on('nick', nick => {
+  socket.on("nick", (nick) => {
     connectedUsers[socket.id] = nick;
-    console.log('new nickname: ' + nick);
-    socket.broadcast.emit('typing', `${connectedUsers[socket.id]} is typing...`);
+    console.log("new nickname: " + nick);
+    socket.broadcast.emit(
+      "typing",
+      `${connectedUsers[socket.id]} is typing...`
+    );
+    console.log(connectedUsers);
   });
 
-  socket.on('typing', isTyping => {
-    console.log('Typing Event: ' + isTyping);
-    if (isTyping)
-    {
-      socket.broadcast.emit('typing', `${connectedUsers[socket.id]} is typing...`);
+  socket.on("priv", ({ msg, dest }) => {
+    console.log("MSG: " + msg);
+    console.log("DEST: " + dest);
+    let dest_socket = null;
+    for (let c in connectedUsers) {
+      if (connectedUsers[c] == dest) {
+        dest_socket = c;
+      }
     }
-    else
-    {
-      socket.broadcast.emit('typing', `No one is typing`);
+    console.log("Dest socket : " + dest_socket);
+    if (dest_socket) {
+      socket
+        .to(dest_socket)
+        .emit("chat message", `${connectedUsers[socket.id]}: ${msg}`);
     }
-  })
+  });
 
-  socket.on('chat message', msg => {
-    console.log('message: ' + msg);
-    socket.broadcast.emit('chat message', `${connectedUsers[socket.id]}: ${msg}`);
+  socket.on("typing", (isTyping) => {
+    console.log("Typing Event: " + isTyping);
+    if (isTyping) {
+      socket.broadcast.emit(
+        "typing",
+        `${connectedUsers[socket.id]} is typing...`
+      );
+    } else {
+      socket.broadcast.emit("typing", `No one is typing`);
+    }
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    socket.broadcast.emit(
+      "chat message",
+      `${connectedUsers[socket.id]}: ${msg}`
+    );
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function () {
+  console.log("listening on *:3000");
 });
