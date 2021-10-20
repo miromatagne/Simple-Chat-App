38e8404d927e53e903230f9edb0b9137f0842469
Saúl Blanco <saul101.pi@outlook.com>
2021-10-20 10:31:13 +0200

    Implemented mandatory tasks

---
+++ index.js
@@ -0,0 +1,50 @@
+const { SocketAddress } = require('net');
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const connectedUsers = {};
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  connectedUsers[socket.id] = 'user';
+  io.emit('chat message', 'Nuevo usuario conectado');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    delete connectedUsers[socket.id];
+  });
+
+  socket.on('nick', nick => {
+    connectedUsers[socket.id] = nick;
+    console.log('new nickname: ' + nick);
+    socket.broadcast.emit('typing', `${connectedUsers[socket.id]} is typing...`);
+  });
+
+  socket.on('typing', isTyping => {
+    console.log('Typing Event: ' + isTyping);
+    if (isTyping)
+    {
+      socket.broadcast.emit('typing', `${connectedUsers[socket.id]} is typing...`);
+    }
+    else
+    {
+      socket.broadcast.emit('typing', `No one is typing`);
+    }
+  })
+
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg);
+    socket.broadcast.emit('chat message', `${connectedUsers[socket.id]}: ${msg}`);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
